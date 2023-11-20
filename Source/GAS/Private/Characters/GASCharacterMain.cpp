2b0f576d5ab2c94a8b120f049c293bfd7ae4d948
DanaFo <x.dfortier@gmail.com>
2023-11-20 15:38:15 -0800

    fixed .h in GASCharacterMain.cpp

--- Source/GAS/Private/Characters/GASCharacterMain.cpp
+++ Source/GAS/Private/Characters/GASCharacterMain.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Characters/GASCharacterMain.h"
+#include "GAS/Public/Characters/GASCharacterMain.h"
 
 // Sets default values
 AGASCharacterMain::AGASCharacterMain()
